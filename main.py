c42e6ab883930b19fe83c362fd479cf3a741181a
Harrison <athertonharrison@gmail.com>
2022-05-16 22:00:25 -0700

    courtesy of @clear-code-projects 38:54

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame, sys
+import pygame, sys, time
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
@@ -7,16 +8,21 @@ class Game:
         # General setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Zelda")
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
                 self.screen.fill("black")
+                self.level.run()
                 pygame.display.update()
                 self.clock.tick(FPS)
 
